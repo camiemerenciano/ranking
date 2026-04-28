6befc18502b7047f8c41e5071691698ed2f435a0
Camila <camilaemerencianolima@gmail.com>
2026-04-28 07:09:06 -0300

    atlz 5

---
+++ concorrencia-app/app/ranking/page.tsx
@@ -0,0 +1,12 @@
+export default function Ranking() {
+  return (
+    <iframe
+      src="/ranking.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  )
+}
